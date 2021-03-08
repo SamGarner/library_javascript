11c16c52fe024c9c51335ad08d7a2f7a1a0fc324
SamGarner <samuel.w.garner@gmail.com>
2021-03-08 17:39:01 -0600

    Feat: Add delete buttons for each book in the library.

--- script.js
+++ script.js
@@ -21,14 +21,44 @@ let addBookToLibrary = () => {
 }
 
 const displayLibrary = (library) => {
-  library.forEach(book => {
+  library.forEach((book, index) => {
     nextBook = document.createElement('LI'); //type of element
-    nextBook.innerHTML = book.info();
+    nextBook.innerHTML = book.info(); //works in backticks
     //bookList.appendChild(document.createElement('LI')); //type of element
     bookList.appendChild(nextBook);
+    deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Remove';
+    deleteButton.className = 'delete-button';
+    deleteButton.dataset.index = index;
+    bookList.appendChild(deleteButton);
     }
   ); //temp
+  addDeleteButtonEvents();
 } 
+
+const addDeleteButtonEvents = () => {
+  deleteButtons = document.querySelectorAll('.delete-button');
+  deleteButtons.forEach(button => button.addEventListener('click', deleteBook));
+}
+
+function deleteBook() {  // can be arrow function?
+  indexToDelete = event.target.dataset.index
+  // remove from Lib array
+  myLibrary.splice(indexToDelete, 1)
+  // remove node and corresponding delete button
+  let oldButton = document.querySelector(`[data-index="${indexToDelete}"]`);  //need the quotes around the index value!
+  let oldBook = document.querySelectorAll('li')[indexToDelete]
+  oldButton.remove();
+  oldBook.remove();
+  refreshButtonIndices();
+}
+
+let refreshButtonIndices = () => {
+  deleteButtons = document.querySelectorAll('.delete-button');
+  deleteButtons.forEach((button,index) => {
+    button.dataset.index = index;
+  })
+};
 
 Book.prototype.info = function() {
   status = this.getReadStatus();
@@ -51,6 +81,5 @@ newBookButton.addEventListener('click', addBookToLibrary);
 myLibrary.push(new Book('HP: Deathly Hallows', 'JK', 760, 'y'));
 myLibrary.push(new Book('A Season with Verona', 'Guy', 303, 'y'));
 myLibrary.push(new Book('The Hobbit', 'Tolkien', 403, 'no'));
-// addBookToLibrary();
-// myLibrary[myLibrary.length -1].info();
+
 displayLibrary(myLibrary);
