0c8e7861682b11d702d49aa1d86ab347b69a8e65
SamGarner <samuel.w.garner@gmail.com>
2021-03-08 11:01:28 -0600

    Feat: Add addBookToLibrary() to get book info from the user and add the new book to myLibrary array.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -5,10 +7,21 @@ function Book(title, author, pages, read) {
   this.read = read;
 };
 
+// take user's input and add the new book to the myLibrary array
+let addBookToLibrary = () => {
+  let title = prompt('What is the name of the book?', 'Moby Dick');
+  let author = prompt('Who is the author?');
+  let pages = prompt('How many pages?');
+  let read = prompt('Have you read it? y/n:', 'y')
+  myLibrary.push(new Book(title, author, pages, read));
+  console.log('Book added!')
+}
+
 Book.prototype.info = function() {
   status = this.getReadStatus();
   console.log(this.title);
   console.log(`${this.title} by ${this.author}, ${this.pages} pages. ${status} `);
+  alert(`${this.title} by ${this.author}, ${this.pages} pages. ${status} `); // temp
 };
 
 Book.prototype.getReadStatus = function() {
@@ -19,3 +32,6 @@ Book.prototype.getReadStatus = function() {
     return 'Has not yet been read.';
   }
 };
+
+addBookToLibrary();
+myLibrary[myLibrary.length -1].info();
